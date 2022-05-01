15be9175d24762f6ada9e89b6ac28aeae45acc8a
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-02 02:04:02 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,81 @@
+const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
+
+const generateJwt = (id, login, role) => {
+    return jwt.sign(
+        {id, login, role},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
 
 class UserController {
     async registration(req, res) {
-        let message = 'It s user registration method'
-        return res.json(message)
+        const {login,
+            password,
+            firs_name,
+            last_name,
+            patronymic,
+            passport
+        } = req.body
+
+        if (!login || !password) {
+            return next(ApiError.badRequest('Некорректный логин или password'))
+        }
+        const candidate = await User.findOne({where: {login}})
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с таким логином уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({login, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJwt(user.id, user.login, user.role)
+        return res.json({token})
+    }
+
+    async registration_admin(req, res) {
+        const {login,
+            password,
+            firs_name,
+            last_name,
+            patronymic,
+            passport,
+            role
+        } = req.body
+
+        if (!login || !password) {
+            return next(ApiError.badRequest('Некорректный логин или password'))
+        }
+        const candidate = await User.findOne({where: {login}})
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с таким логином уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({login, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJwt(user.id, user.login, user.role)
+        return res.json({token})
     }
 
     async login(req, res) {
-        let message = 'It s user login method'
-        return res.json(message)
-    }
-
-    async changeRole(req, res) {
-        const {login} = req.params
-
-        let message = `It s user changeRole ${login.toString()} method`
-        return res.json(message)
+        const {login, password} = req.body
+        const user = await User.findOne({where: {login}})
+        if (!user) {
+            return next(ApiError.internal('Пользователь не найден'))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal('Указан неверный пароль'))
+        }
+        const token = generateJwt(user.id, user.login, user.role)
+        return res.json({token})
     }
 
     async check(req, res) {
-        let message = 'It s user ckeck method'
-        return res.json(message)
+        const token = generateJwt(req.user.id, req.user.login, req.user.role)
+        return res.json({token})
     }
 }
 
