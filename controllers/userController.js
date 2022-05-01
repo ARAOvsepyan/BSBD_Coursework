5c8cdcc516e136d78d281fa32a775048a47ced0c
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-01 18:57:21 +0300

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,25 @@
 
 class UserController {
     async registration(req, res) {
-        return res.json()
+        let message = 'It s user registration method'
+        return res.json(message)
     }
 
     async login(req, res) {
-
-        return res.json()
+        let message = 'It s user login method'
+        return res.json(message)
     }
 
     async changeRole(req, res) {
         const {login} = req.params
-           
-        return res.json()
+
+        let message = `It s user changeRole ${login.toString()} method`
+        return res.json(message)
     }
 
     async check(req, res) {
-
-        return res.json()
+        let message = 'It s user ckeck method'
+        return res.json(message)
     }
 }
 
