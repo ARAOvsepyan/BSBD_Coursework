500236f1b5ac1ddb83e50a4cb084da7fb2b02f07
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-02 02:04:13 +0300

    Update feedingRouter.js

--- router/feedingRouter.js
+++ router/feedingRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const feedingController = require('../controllers/feedingController')
+const checkRole = require('../middleware/checkMiddleware')
 
-router.post('/', feedingController.create)
+router.post('/', checkRole('ADMIN'), feedingController.create)
 router.get('/:type', feedingController.getByType)
 
 module.exports = router
