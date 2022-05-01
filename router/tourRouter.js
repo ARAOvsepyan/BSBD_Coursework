82543cb3cce4082894c305fd098c6eefa4c9ca05
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-02 02:04:26 +0300

    Update tourRouter.js

--- router/tourRouter.js
+++ router/tourRouter.js
@@ -1,9 +1,11 @@
 const Router = require('express')
 const router = new Router()
 const tourController = require('../controllers/tourController')
+const checkRole = require('../middleware/checkMiddleware')
 
-router.post('/', tourController.create)
+router.post('/', checkRole('ADMIN'), tourController.create)
 router.get('/', tourController.getAll)
 router.get('/:id', tourController.getOne)
+router.delete('/:tour_name', checkRole('ADMIN'), tourController.delete)
 
 module.exports = router
