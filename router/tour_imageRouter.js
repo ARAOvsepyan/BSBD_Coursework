31098ee62511a85ddfa08e14d96c1b6a25457077
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-02 02:03:52 +0300

    Delete tour_imageRouter.js

--- router/tour_imageRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const tourController = require('../controllers/tourController')
-
-router.post('/', tourController.create)
-router.get('/', tourController.getAll)
-router.get('/:id', tourController.getOne)
-
-module.exports = router
