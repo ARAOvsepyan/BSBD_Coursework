f92ae8d6d625e641020acd9d617fb4eb19b1df8d
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-02 02:04:18 +0300

    Update reductionRouter.js

--- router/reductionRouter.js
+++ router/reductionRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const reducionController = require('../controllers/reductionController')
+const checkRole = require('../middleware/checkMiddleware')
 
-router.post('/', reducionController.create)
+router.post('/', checkRole('ADMIN'), reducionController.create)
 router.get('/:amount', reducionController.getByAmount)
 
 module.exports = router
