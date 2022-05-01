7922ac669ef8824fbc8dd5bb0c44914484b95c70
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-02 02:04:28 +0300

    Update userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,10 +1,12 @@
 const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
+const checkRole = require('../middleware/checkMiddleware')
+const authMiddleware = require('../middleware/authMiddleware')
 
 router.post('/registration', userController.registration)
+router.post('/registration_admin', checkRole('ADMIN'), userController.registration_admin)
 router.post('/login', userController.login)
-router.post('/:login', userController.changeRole)
-router.get('/auth', userController.check)
+router.get('/auth', authMiddleware, userController.check)
 
 module.exports = router
