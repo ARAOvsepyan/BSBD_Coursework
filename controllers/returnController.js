a4cb04f92f3e8fd18a623261915dd8763862a4a9
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-04 04:05:38 +0300

    Update returnController.js

--- controllers/returnController.js
+++ controllers/returnController.js
@@ -1,44 +1,56 @@
 const {Return, Sale} = require('../models/models')
+const ApiError = require('../error/ApiError')
 
 class ReturnController {
-    async update(req, res) {
-        let {date, quantity, userId, cause} = req.body
-        
-        const sale_q = await Sale.findAll({where: {userId}})
-        sale_q[0].quantity -= quantity
-
-        if (sale_q[0].quantity <= 0) {
-            const sale = await Sale.update({
-                date: null,
-                quantity: null,
-                tourId: null
-            },
-            {
-                where: {userId}
-            })
-
-            const ret = await Return.create({
-                date,
-                quantity,
-                cause,
-                userId,
-                saleId: sale_q[0].id,
-                tourId: sale_q[0].tourId
-            })
+    async update(req, res, next) {
+        try {
+            let {date, quantity, userId, cause} = req.body
             
-            return res.json(ret)
-        } else {
-            const sale = await Sale.update({
-                date: date,
-                quantity: sale_q[0].quantity,
-            },
-            {
-                where: {userId}
-            })
-        }
+            const sale_q = await Sale.findAll({where: {userId}})
+            sale_q[0].quantity -= quantity
 
+            if (sale_q[0].quantity <= 0) {
+                const sale = await Sale.update({
+                    date: null,
+                    quantity: null,
+                    tourId: null
+                },
+                {
+                    where: {userId}
+                })
+
+                const ret = await Return.create({
+                    date,
+                    quantity,
+                    cause,
+                    userId,
+                    saleId: sale_q[0].id,
+                    tourId: sale_q[0].tourId
+                })
+                
+                return res.json(ret)
+            } else {
+                const sale = await Sale.update({
+                    date: date,
+                    quantity: sale_q[0].quantity,
+                },
+                {
+                    where: {userId}
+                })
+            }
+
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
     }
 
+    async get(req, res) {
+        let {userId} = req.params
+
+        const ret = await Return.findAll({where: {userId}})
+
+        return res.json(ret)
+    }
 }
 
 module.exports = new ReturnController()
