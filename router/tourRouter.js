8bed003ba1c6a71e98fd94f56e820958a8772080
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-04 04:00:06 +0300

    Update tourRouter.js

--- router/tourRouter.js
+++ router/tourRouter.js
@@ -4,6 +4,7 @@ const tourController = require('../controllers/tourController')
 const checkRole = require('../middleware/checkMiddleware')
 
 router.post('/', checkRole('ADMIN'), tourController.create)
+router.post('/create_img', checkRole('ADMIN'), tourController.create_img)
 router.get('/', tourController.getAll)
 router.get('/:id', tourController.getOne)
 router.delete('/:tour_name', checkRole('ADMIN'), tourController.delete)
