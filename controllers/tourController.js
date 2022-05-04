c7e7e56c7c2be55d219b42ff8963a03ad70866f0
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-04 04:00:15 +0300

    Update tourController.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,6 @@
 const uuid = require('uuid')
 const path = require('path');
-const {Tour} = require('../models/models')
+const {Tour, Country, Feeding, Reduction, Tour_img} = require('../models/models')
 const ApiError = require('../error/ApiError');
 
 class TourController {
@@ -15,14 +15,10 @@ class TourController {
                 children,
                 days,
                 nights,
-                countyId,
-                feedinfId,
+                countryId,
+                feedingId,
                 reductionId,
             } = req.body
-
-            const {img} = req.files
-            let fileName = uuid.v4() + ".jpg"
-            img.mv(path.resolve(__dirname, '..', 'static', fileName))
 
             const tour = await Tour.create({
                 tour_name,
@@ -33,9 +29,8 @@ class TourController {
                 children,
                 days,
                 nights,
-                img: fileName,
-                countyId,
-                feedinfId,
+                countryId,
+                feedingId,
                 reductionId,
             });
             
@@ -46,36 +41,67 @@ class TourController {
 
     }
 
+    async create_img(req, res, next) {
+        try {
+            let {tourId} = req.body
+
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname, '..', 'static', fileName))
+
+            const tour_img = await Tour_img.create({tourId ,img: fileName})
+
+            return res.json(tour_img)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
+
+    }
+
     async getAll(req, res) {
-        let tour = await Tour.findAll()
+        let tour = await Tour.findAll({attributes: ["id","tour_name", "price", "date", "dep_city", "adilts", "children", "days", "nights"],
+            include: [
+            {
+                model: Country,
+                attributes: ["name"]
+            },
+            {
+                model: Feeding,
+                attributes: ["type"]
+            },
+            {
+                model: Reduction,
+                attributes: ["amount"]
+            }
+        ]})
+
         return res.json(tour)
     }
 
     async getOne(req, res) {
         const {id} = req.params
 
-        let tour = await Tour.findAll({where: {id}})
+        let tour = await Tour.findAll({attributes: ["id","tour_name", "price", "date", "dep_city", "adilts", "children", "days", "nights"],
+            include: 
+            [{
+                model: Country,
+                attributes: ["name"]
+            },
+            {
+                model: Feeding,
+                attributes: ["type"]
+            },
+            {
+                model: Reduction,
+                attributes: ["amount"]
+            }],
+            where: {id}
+        })
         return res.json(tour)
     }
 
     async delete(req, res) {
-        const {tour_name} = req.params
-
-        try {
-            const tour = await Tour.findAll({
-                attributes: ['img'], 
-                where: {tour_name: tour_name}
-            })
-    
-            
-            fs.unlink(path.resolve(__dirname, '..', 'static', tour[0].img))
-            
-            const tour_res = await Tour.destroy({where: {tour_name: tour_name}})
-
-            return res.json(tour_res)
-        } catch (e) {
-            return next(ApiError.internal('Такого поста не существует'))
-        }      
+     
     }
 
 }
