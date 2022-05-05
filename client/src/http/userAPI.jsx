60a8652e7c072dda02b308f67a483cf4d8e371c1
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-05 19:46:50 +0300

    Create userAPI.jsx

---
+++ client/src/http/userAPI.jsx
@@ -0,0 +1,27 @@
+import {$authHost, $host} from "./index";
+import jwt_decode from  'jwt-decode'
+
+export const registration = async (login, password, firs_name, last_name, patronymic, passport) => {
+    const {data} = await $host.post('api/user/registration', {
+        login,
+        password,
+        firs_name,
+        last_name,
+        patronymic,
+        passport
+    })
+    localStorage.setItem('token', data.token)
+    return jwt_decode(data.token)
+}
+
+export const login = async (login, password) => {
+    const {data} = await $host.post('api/user/login', {login, password})
+    localStorage.setItem('token', data.token)
+    return jwt_decode(data.token)
+}
+
+export const check = async () => {
+    const {data} = await $authHost.get('api/user/auth' )
+    localStorage.setItem('token', data.token)
+    return jwt_decode(data.token)
+}
