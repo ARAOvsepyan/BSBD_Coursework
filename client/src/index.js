2735c5a4f63095816a30f82d60c1251efbde4885
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-05 19:46:52 +0300

    Create index.js

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+import React, {createContext} from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import TourStore from './store/tourStore';
+import UserStore from './store/userStore';
+
+export const Context = createContext(null)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Context.Provider value={{
+      user: new UserStore(),
+      tour: new TourStore()
+    }}>
+    <App />
+  </Context.Provider>,
+);
+
