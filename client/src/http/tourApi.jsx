4f12f7dd785b901456a84f01e61a253cc5584c6d
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-05 19:46:48 +0300

    Create tourApi.jsx

---
+++ client/src/http/tourApi.jsx
@@ -0,0 +1,17 @@
+import {$authHost, $host} from "./index";
+
+export const createTour = async (tour) => {
+    const {data} = await $authHost.post('api/tour', tour)
+    return data
+}
+
+export const deleteTour = async (title) => {
+    const {data} = await $authHost.post('api/tour/' + title)
+    return data
+}
+
+export const fetchTour = async () => {
+    const {data} = await $host.get('api/tour/')
+    return data
+}
+
