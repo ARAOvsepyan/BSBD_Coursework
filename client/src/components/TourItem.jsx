98b78964249569576d891230af8722e468f0f730
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-08 13:00:51 +0300

    Update TourItem.jsx

--- client/src/components/TourItem.jsx
+++ client/src/components/TourItem.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
-import { Container, Col, Card } from 'react-bootstrap';
+import { Container, Card } from 'react-bootstrap';
+import { useNavigate } from "react-router";
+import { TOUR_ROUTE } from '../utils/consts';
 
 const TourItem = ({tour}) => {
-    return (
-      <Container
-            className="d-flex justify-content-center align-items-center"
-      >
-        <Card className='m-4' style={{width: 600, cursor: 'pointer'}} border={"light"}>
-          <Card.Img variant="top" src={'http://localhost:8000/' + tour.img} />
-          <Card.Body>
-            <Card.Title>{tour.tour_name}</Card.Title>
-            <Card.Text>
-                {tour.text}
-                {tour.dep_city} - {tour.country.name} <br/>
-                Цена: {tour.reduction.amount > 0 ? 
-                  (tour.price*tour.reduction.amount)/100 
-                  : 
-                  tour.price
-                  } <br/>
-                Склидка: {tour.reduction.amount + '%'}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Container>
-    );
+  const navigate = useNavigate()
+
+  return (
+    <Container
+          className="d-flex justify-content-center align-items-center"
+    >
+      <Card className='m-4' style={{width: 600, cursor: 'pointer'}} border={"light"} onClick={() => navigate(TOUR_ROUTE + '/' + tour.id)}>
+        <Card.Img variant="top" src={'http://localhost:8000/' + tour.img} />
+        <Card.Body>
+          <Card.Title>{tour.tour_name}</Card.Title>
+          <Card.Text>
+              {tour.text}
+              {tour.dep_city} - {tour.country.name} <br/>
+              Цена: {tour.reduction.amount > 0 ? 
+                (tour.price*tour.reduction.amount)/100 
+                : 
+                tour.price
+                } <br/>
+              Скидка: {tour.reduction.amount + '%'}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
 };
 
 export default TourItem;
