fbaf23afc50e336091d066302ce51276797a8e7d
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-09 00:57:00 +0300

    Create DeleteTour.jsx

---
+++ client/src/components/modals/DeleteTour.jsx
@@ -0,0 +1,62 @@
+import React, {useContext, useState, useEffect} from 'react';
+import Modal from "react-bootstrap/Modal";
+import { Button, Dropdown} from "react-bootstrap";
+import { Context } from '../..';
+import { deleteTour, fetchTour } from '../../http/tourApi';
+
+const DeleteTour = ({show, onHide}) => {
+    const {tour} = useContext(Context)
+    const [title, setTitle] = useState('')
+
+
+    useEffect(() => {
+        fetchTour().then(data => {tour.setTour(data)})
+    }, [])
+
+    const removeTure = () => {
+        try {
+            deleteTour(title).then(() => {
+                setTitle('')
+                onHide()
+            })
+        } catch (error) {
+            alert(error)
+        }
+        
+    }
+
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Добавить тип
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Dropdown className="mt-2 mb-2">
+                        <Dropdown.Toggle>{title || 'Выберите тур'}</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {tour.tour.map(type =>
+                                <Dropdown.Item
+                                    onClick={() => setTitle(type.tour_name)}
+                                    key={type.id}
+                                >
+                                    {type.tour_name}
+                                </Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
+                <Button variant="outline-success" onClick={removeTure}>Удалить</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default DeleteTour;
