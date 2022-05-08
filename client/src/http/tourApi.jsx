905bf0f59a74acd8c6a84fadbea0a40439d4756d
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-09 00:56:48 +0300

    Update tourApi.jsx

--- client/src/http/tourApi.jsx
+++ client/src/http/tourApi.jsx
@@ -19,3 +19,33 @@ export const fetchOneTour = async (id) => {
     const {data} = await $host.get('api/tour/' + id)
     return data
 }
+
+export const fetchCountry = async () => {
+    const {data} = await $host.get('api/country/')
+    return data
+}
+
+export const fetchFeeding = async () => {
+    const {data} = await $host.get('api/feeding/')
+    return data
+}
+
+export const fetchReduction = async () => {
+    const {data} = await $host.get('api/reduction/')
+    return data
+}
+
+export const createCountry = async (name) => {
+    const {data} = await $authHost.post('api/country/', name)
+    return data
+}
+
+export const createFeeding = async (name) => {
+    const {data} = await $authHost.post('api/feeding/', name)
+    return data
+}
+
+export const createReduction = async (name) => {
+    const {data} = await $authHost.post('api/reduction/', name)
+    return data
+}
