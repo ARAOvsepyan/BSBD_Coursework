93412070fff85895a4be966afb9be9387f734a14
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-09 00:56:36 +0300

    Update reductionController.js

--- server/controllers/reductionController.js
+++ server/controllers/reductionController.js
@@ -2,17 +2,22 @@ const {Reduction} = require('../models/models')
 
 class ReductionController {
     async create(req, res) {
-        let {type, amount} = req.body
+        let {amount} = req.body
+        let type
+
+        if (amount > 0) {
+            type = true
+        } else {
+            type = false
+        }
 
         const reduction = await Reduction.create({type, amount})
 
         return res.json(reduction)
     }
 
-    async getByAmount(req, res) {
-        let {amount} = req.params
-
-        let reduction = await Reduction.findAll({where: {amount}})
+    async getAll(req, res) {
+        let reduction = await Reduction.findAll()
 
         return res.json(reduction)
     }
