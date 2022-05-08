51d0233683d045c0fb358ed95ac4caa5b5c27ea0
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-09 00:56:56 +0300

    Create UpdateTour.jsx

---
+++ client/src/components/modals/UpdateTour.jsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import Modal from "react-bootstrap/Modal";
+import {Form, Button} from "react-bootstrap";
+
+const UpdateTour = ({show, onHide}) => {
+    const [value, setValue] = useState('')
+
+    const addTure = () => {
+        
+    }
+
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Добавить тип
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Control
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                        placeholder={"Введите название типа"}
+                    />
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
+                <Button variant="outline-success" onClick={addTure}>Добавить</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+};
+
+export default UpdateTour;
