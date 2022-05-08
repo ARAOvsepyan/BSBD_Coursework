eb98dfd79b244aad7b753212a374b55b2cda6261
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-09 00:56:42 +0300

    Update TourPage.jsx

--- client/src/pages/TourPage.jsx
+++ client/src/pages/TourPage.jsx
@@ -1,32 +1,35 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
 import {Context} from '..'
-import {useParams} from 'react-router-dom'
+import {useNavigate, useParams} from 'react-router-dom'
+import { LOGIN_ROUTE } from '../utils/consts';
 import {fetchOneTour} from '../http/tourApi'
 import { buyTour } from '../http/saleApi';
 
 const TourPage = () => {
     const {user} = useContext(Context)
+    const navigate = useNavigate()
+
     const [tour, setTour] = useState([])
     const [amount, setAmount] = useState('')
     const [country, setCountry] = useState('')
     const [feeding, setFeeding] = useState('')
+    let [quantity, setQuantity] = useState(0)
     const {id} = useParams()
 
     const add = () => {
         const info = new FormData()
-        info.append('date', '26.07.1999')
-        info.append('quantity', 1)
+        info.append('quantity', quantity)
         info.append('userId', user._user.user.id)
         info.append('tourId',  tour.id)
         try {  
-            buyTour(info)
+            buyTour(info).then(alert('Покупка прошла успешно'))
         } catch (error) {
           alert('Что то пошло не так')
         }
     }
     
-
+   
     useEffect(() => {
         fetchOneTour(id).then(data => {
             setTour(data)
@@ -58,7 +61,16 @@ const TourPage = () => {
                             tour.price
                             } руб.</h3>
                             Скидка: {amount + '%'}
-                            <Button variant={"outline-dark"} onClick={add}>Купить</Button>
+                            <td>
+                                <span className="btn btn-primary" style={{ margin: '2px' }} onClick={()=>setQuantity(quantity-=1)}>-</span>
+                                <span className="btn btn-info">{quantity}</span>
+                                <span className="btn btn-primary" style={{ margin: '2px' }} onClick={()=>setQuantity(quantity+=1)}>+</span>
+                            </td>
+                            {user.isAuth === 'USER' ? 
+                                <Button variant={"outline-dark"} onClick={add}>Купить</Button>
+                                :
+                                <Button variant={"outline-dark"} onClick={() => navigate(LOGIN_ROUTE)}>Авторизуйтес что бы купить</Button>
+                            }
                         </Card>
                     </Col>
                     </Row>
