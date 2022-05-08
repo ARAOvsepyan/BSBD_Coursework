b17b153a45d6e445e47c905c7beedffe67a016c2
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-08 13:01:00 +0300

    Create saleApi.jsx

---
+++ client/src/http/saleApi.jsx
@@ -0,0 +1,11 @@
+import {$authHost} from "./index";
+
+export const buyTour = async (info) => {
+    const {data} = await $authHost.post('api/sale', info)
+    return data
+}
+
+export const fetchSale = async (id) => {
+    const {data} = await $authHost.get('api/sale/' + id)
+    return data
+}
