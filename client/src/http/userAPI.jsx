db22cd37c28e933ef4151309c95b1c76eb936257
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-09 00:56:46 +0300

    Update userAPI.jsx

--- client/src/http/userAPI.jsx
+++ client/src/http/userAPI.jsx
@@ -14,6 +14,18 @@ export const registration = async (login, password, firs_name, last_name, patron
     return jwt_decode(data.token)
 }
 
+export const registration_admin = async (login, password, firs_name, last_name, patronymic, passport) => {
+    const {data} = await $authHost.post('api/user/registration_admin', {
+        login,
+        password,
+        firs_name,
+        last_name,
+        patronymic,
+        passport
+    })
+    return jwt_decode(data.token)
+}
+
 export const login = async (login, password) => {
     const {data} = await $host.post('api/user/login', {login, password})
     localStorage.setItem('token', data.token)
