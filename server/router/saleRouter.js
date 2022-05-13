39ece493f4c99c748c64447e8926d9b4bdc74c34
Ara Ovsepyan <ara07.99@icloud.com>
2022-05-14 00:55:18 +0300

    Update saleRouter.js

--- server/router/saleRouter.js
+++ server/router/saleRouter.js
@@ -2,8 +2,11 @@ const Router = require('express')
 const router = new Router()
 const saleController = require('../controllers/saleController')
 const authMiddleware = require('../middleware/authMiddleware')
+const checkRole = require('../middleware/checkMiddleware')
 
 router.post('/', authMiddleware, saleController.update)
 router.get('/:userId', authMiddleware, saleController.get)
+router.get('/', checkRole('ADMIN'), saleController.getAll)
+
 
 module.exports = router
